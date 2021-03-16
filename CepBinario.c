e5c9a575e0870c3ec8153b3303e205506b2e5aa8
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-03-16 01:44:21 -0300

    Adicionando tarefa

---
+++ CepBinario.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct _Endereco Endereco;
+
+struct _Endereco {
+	char logradouro[72];
+	char bairro[72];
+	char cidade[72];
+	char uf[72];
+	char sigla[2];
+	char cep[8];
+	char lixo[2];
+};
+
+int main(int argc, char**argv) {
+	FILE *f;
+	Endereco e;
+	long tamanhoArquivo, posicao = -1, primeiro, ultimo, meio;
+
+	if(argc != 2)	{
+		fprintf(stderr, "USO: %s [CEP]", argv[0]);
+		return 1;
+	}
+
+	f = fopen("cep_ordenado.dat","r");
+	fseek(f,0,SEEK_END);
+
+	tamanhoArquivo = ftell(f);
+	rewind(f); // fseek(f,0,SEEK_SET);
+
+	primeiro = 0;
+	ultimo = (tamanhoArquivo/sizeof(Endereco))-1;
+	meio = (primeiro+ultimo)/2;
+
+	while(ftell(f) != posicao)	{
+		posicao = ftell(f);
+		fseek(f, meio * sizeof(Endereco), SEEK_SET);	
+		fread(&e,sizeof(Endereco),1,f);
+
+		if(strncmp(argv[1], e.cep, 8) == 0) {
+			printf("Achado: \n%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n", e.logradouro, e.bairro, e.cidade, e.uf, e.sigla, e.cep);
+		} else if(strncmp(argv[1], e.cep, 8) < 0){
+			ultimo = meio;
+			meio = (primeiro+ultimo)/2;
+		} else if(strncmp(argv[1], e.cep, 8) > 0){
+			primeiro = meio;
+			meio = (primeiro+ultimo)/2;
+		}
+	}
+
+	fclose(f);
+	return 0;
+}
