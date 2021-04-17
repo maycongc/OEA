b5cff00aa72fa9838212682d88980b6d6e7cee84
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-04-17 03:51:03 -0300

    feat: add index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path')
+const loadEnderecos = require('./loadEnderecos')
+const { mergeSort } = require('./mergeSort')
+const mergeBlocks = require('./mergeBlocks')
+
+const main = async () => {
+  let dir = resolve(__dirname, '..', 'cep.dat')
+  const e = await loadEnderecos(dir)
+  await mergeSort(e, 1)
+  await mergeBlocks()
+}
+
+main()
